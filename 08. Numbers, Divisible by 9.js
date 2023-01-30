12e081aa32641a5dccd2c020274bfe5b2dbd4e3f
M-Gabrova <milenagabrova@gmail.com>
2023-01-30 20:06:52 +0200

    first commit

---
+++ 08. Numbers, Divisible by 9.js
@@ -0,0 +1,15 @@
+function numbersDivisableBy9 (input){
+    let startNum = Number(input[0]);
+    let endNum = Number(input[1]);
+    let sum = 0;
+    let buff ="";
+    
+    for (let i = startNum; i <= endNum; i++) {
+        if(i % 9 == 0)
+        sum += i;
+        buff += i + "\n";
+    }
+    console.log(`The sum: ${sum}`);
+    console.log(buff);
+}
+numbersDivisableBy9 (["100", "200"])
